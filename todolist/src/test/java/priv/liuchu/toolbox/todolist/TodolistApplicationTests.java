27cf6f2c672cb00cadeae513f469f8301e3d5086
liuchu <liuchuu@126.com>
2021-07-02 21:38:39 +0800

    add maven wrapper files

---
+++ todolist/src/test/java/priv/liuchu/toolbox/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.liuchu.toolbox.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
