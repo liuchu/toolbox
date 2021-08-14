c3d220fe9179843228cd5a7f6bf0ba7ca7585785
liuchu <liuchuu@126.com>
2021-08-14 16:03:14 +0800

    添加必要模块-2

--- todolist/src/main/java/priv/liuchu/toolbox/todolist/TodolistApplication.java
+++ todolist/src/main/java/priv/liuchu/toolbox/todolist/TodolistApplication.java
@@ -1,18 +1,13 @@
 package priv.liuchu.toolbox.todolist;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableJpaRepositories("priv.liuchu.toolbox.todolist.dao")
-@EnableDiscoveryClient
 public class TodolistApplication {
 
     public static void main(String[] args) {
--- toolbox-web/src/main/java/priv/liuchu/toolbox/web/ToolboxWebApplication.java
+++ toolbox-web/src/main/java/priv/liuchu/toolbox/web/ToolboxWebApplication.java
@@ -2,8 +2,10 @@ package priv.liuchu.toolbox.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = {"priv.liuchu.toolbox.todolistapi"})
 public class ToolboxWebApplication {
 
     public static void main(String[] args) {
